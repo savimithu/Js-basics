e2f56b958ed853390006a1c94bb4361e41feb3e8
savitha <savimithu@gmail.com>
2022-12-14 17:00:12 -0600

    Name chnage

---
+++ jsbasics01.js
@@ -0,0 +1,20 @@
+ int number 1 , int number 2 ;
+ sc = new ServiceWorkerContainer(system.int);
+ system . out. print (number:);
+ number 1 = sc.nextInt(5678774);
+ system . out. print (number:);
+ number 2 = sc.nextInt(5674867);
+ if (number 1>number2)
+ {
+    system.out.println('\n The largest number = +number 1');
+node 
+ }
+ else
+ {
+    system.out.println('\n the largest number = +number 2');
+ }
+ 
+ 
+
+
+
